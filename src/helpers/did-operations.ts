531fc2c6c6556d30be7f826c4ad016bfbca1b8de
Dracoonghost <46518813+Dracoonghost@users.noreply.github.com>
2022-09-03 15:01:13 +0530

    bls vc added

--- src/helpers/did-operations.ts
+++ src/helpers/did-operations.ts
@@ -1,72 +1,111 @@
-import { keyOps } from './key-operation'
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { KeyOps } from './key-operation'
 
-export class didOps {
+export class DidOps {
   static async generateDIDDocument(did: string, type: string): Promise<any> {
-    const keyPair = await keyOps.generateKeyPair(type)
-    const id = `did:volary:${did}`
-    return {
-      didDoc: {
-        '@context': 'https://w3id.org/did/v1',
-        id,
-        publicKey: [
-          {
-            id: `${did}#keys-1`,
-            type:
-              type === 'bls12381g2'
-                ? 'Bls12381G2Key2020'
-                : 'Ed25519VerificationKey2018',
-            owner: did,
-            publicKeyBase64: keyPair.publicKey,
-          },
-        ],
-      },
-      privateKeyBase64: keyPair.secretKey,
+    try {
+      const keyPair = await KeyOps.generateKeyPair(type)
+      const id = `did:volary:${did}`
+      return {
+        status: 1,
+        didDoc: {
+          '@context': 'https://w3id.org/did/v1',
+          id,
+          publicKey: [
+            {
+              id: `${did}#keys-1`,
+              type:
+                type === 'bls12381g2'
+                  ? 'Bls12381G2Key2020'
+                  : 'Ed25519VerificationKey2018',
+              owner: did,
+              publicKeyBase64: keyPair.publicKey,
+            },
+          ],
+        },
+        privateKeyBase64: keyPair.secretKey,
+      }
+    } catch (error) {
+      return {
+        status: 0,
+        error,
+      }
     }
   }
 
-  static issueVC(
+  static async issueVC(
     issuerDid: string,
     claims: any,
     vcType: string,
     issuerName: string,
     subjectDID: string,
-    secretKey: any
-  ): any {
-    const signatures: any[] = []
-    Object.entries(claims).forEach((claim) => { 
-      const claimKey = claim[0]
-      const clainValue = claim[1]
-      const objectToSign: any = {}
-      objectToSign[claimKey] = clainValue
-      const signature = keyOps.sign(JSON.stringify(objectToSign), secretKey)
-      signatures.push({
-        type: 'Ed25519Signature2018',
-        created: new Date().toISOString(),
-        creator: issuerDid,
-        signatureValueBase64: signature,
-        claimId: `${subjectDID}#${claim[0]}`,
-      })
-    })
-    const credentialSubject = claims
-    credentialSubject['id'] = subjectDID
-    return {
-      vc: {
-        '@context': 'https://w3id.org/did/v1',
-        issuer: {
-          id: issuerDid,
-          issuerName,
+    keypair: any,
+    type: any
+  ): Promise<any> {
+    try {
+      if (type === 'bls12381g2') {
+        const messages: string[] = []
+        Object.entries(claims).forEach((claim) => {
+          const claimKey = claim[0]
+          const clainValue = claim[1]
+          const objectToSign: any = {}
+          objectToSign[claimKey] = clainValue
+          messages.push(JSON.stringify(objectToSign))
+        })
+        const signedClaim = await KeyOps.blssign(messages, keypair)
+        const credentialSubject = claims
+        credentialSubject['id'] = subjectDID
+        return {
+          status: 1,
+          vc: {
+            '@context': 'https://w3id.org/did/v1',
+            issuer: {
+              id: issuerDid,
+              issuerName,
+            },
+            credentialSubject,
+            proof: {
+              type: 'BbsBlsSignature2020',
+              created: new Date().toISOString(),
+              creator: issuerDid,
+              proofOfPurpose: 'assertionMethod',
+              verificationMethod: issuerDid,
+              value: signedClaim,
+            },
+            type: ['VerifiableCredential', vcType],
+          },
+        }
+      }
+      const signedClaim = KeyOps.sign(JSON.stringify(claims), keypair.secretKey)
+      const credentialSubject = claims
+      credentialSubject['id'] = subjectDID
+      return {
+        status: 1,
+        vc: {
+          '@context': 'https://w3id.org/did/v1',
+          issuer: {
+            id: issuerDid,
+            issuerName,
+          },
+          credentialSubject,
+          proof: {
+            type: 'Ed25519Signature2018',
+            created: new Date().toISOString(),
+            creator: issuerDid,
+            proofOfPurpose: 'assertionMethod',
+            verificationMethod: issuerDid,
+            value: signedClaim,
+          },
+          type: ['VerifiableCredential', vcType],
         },
-        credentialSubject,
-        proof: {
-          type: 'Ed25519Signature2018',
-          created: new Date().toISOString(),
-          creator: issuerDid,
-          proofOfPurpose: 'assertionMethod',
-          verificationMethod: issuerDid,
-          signatures,
-        },
-        type: ['VerifiableCredential', vcType],
-      },
+      }
+    } catch (error) {
+      console.log(error)
+      return {
+        status: 0,
+        error,
+      }
     }
   }
 }
--- src/helpers/key-operation.ts
+++ src/helpers/key-operation.ts
@@ -1,8 +1,26 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import nacl from 'tweetnacl'
 import util from 'tweetnacl-util'
-
-export class keyOps {
-  static generateKeyPair(): any {
+import {
+  generateBls12381G2KeyPair,
+  blsSign,
+  blsVerify,
+  blsCreateProof,
+  blsVerifyProof,
+  BlsKeyPair,
+  BbsVerifyResult,
+} from '@mattrglobal/bbs-signatures'
+export class KeyOps {
+  static async generateKeyPair(
+    type: string
+  ): Promise<{ publicKey: string; secretKey: string }> {
+    if (type === 'bls12381g2') {
+      const keyPair = await generateBls12381G2KeyPair()
+      return {
+        publicKey: util.encodeBase64(keyPair.publicKey),
+        secretKey: util.encodeBase64(keyPair.secretKey),
+      }
+    }
     const keyPair = nacl.sign.keyPair()
     return {
       publicKey: util.encodeBase64(keyPair.publicKey),
@@ -16,6 +34,80 @@ export class keyOps {
       util.decodeBase64(secretKey)
     )
     return util.encodeBase64(signature)
+  }
+
+  static async blsCreateProof(
+    messages: [string],
+    publicKey: string,
+    signature: string,
+    nonce: string,
+    revealed: []
+  ): Promise<string> {
+    const uMessages: Uint8Array[] = []
+    messages.forEach((message) => {
+      uMessages.push(Uint8Array.from(Buffer.from(message, 'utf-8')))
+    })
+    const proof = await blsCreateProof({
+      signature: util.decodeBase64(signature),
+      publicKey: util.decodeBase64(publicKey),
+      messages: uMessages,
+      nonce: Uint8Array.from(Buffer.from(nonce, 'utf8')),
+      revealed,
+    })
+    return util.encodeBase64(proof)
+  }
+
+  static async blsVerifyProof(
+    messages: [string],
+    publicKey: string,
+    nonce: string,
+    proof: string
+  ): Promise<BbsVerifyResult> {
+    const uMessages: Uint8Array[] = []
+    messages.forEach((message) => {
+      uMessages.push(Uint8Array.from(Buffer.from(message, 'utf-8')))
+    })
+    const verified = await blsVerifyProof({
+      publicKey: util.decodeBase64(publicKey),
+      messages: uMessages,
+      nonce: Uint8Array.from(Buffer.from(nonce, 'utf8')),
+      proof: util.decodeBase64(proof),
+    })
+    return verified
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  static async blssign(messages: string[], keyPair: any): Promise<string> {
+    const uMessages: Uint8Array[] = []
+    messages.forEach((message) => {
+      uMessages.push(Uint8Array.from(Buffer.from(message, 'utf-8')))
+    })
+    const decodedKeyPair: BlsKeyPair = {
+      publicKey: util.decodeBase64(keyPair.publicKey),
+      secretKey: util.decodeBase64(keyPair.secretKey),
+    }
+    const signature = await blsSign({
+      keyPair: decodedKeyPair,
+      messages: uMessages,
+    })
+    return util.encodeBase64(signature)
+  }
+
+  static async blsverify(
+    messages: [string],
+    publicKey: string,
+    signature: string
+  ): Promise<BbsVerifyResult> {
+    const uMessages: Uint8Array[] = []
+    messages.forEach((message) => {
+      uMessages.push(Uint8Array.from(Buffer.from(message, 'utf-8')))
+    })
+    const isVerified = await blsVerify({
+      publicKey: util.decodeBase64(publicKey),
+      messages: uMessages,
+      signature: util.decodeBase64(signature),
+    })
+    return isVerified
   }
 
   static verify(
